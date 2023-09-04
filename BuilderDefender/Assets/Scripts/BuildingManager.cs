3e2eeeaa620d09ee4c790cefea0621de84642af3
NicolasWiedel <nicolaswiedel@googlemail.com>
2023-09-04 08:21:20 +0200

    3. Placing objects

---
+++ BuilderDefender/Assets/Scripts/BuildingManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingManager : MonoBehaviour
+{
+    [SerializeField]
+    private Transform mouseVisualTransform;
+
+    private Camera mainCamera;
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    void Update()
+    {
+        mouseVisualTransform.position = GetMousePosition();
+    }
+
+    private Vector3 GetMousePosition()
+    {
+        Vector3 mouseWorldPosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        mouseWorldPosition.z = 0f;
+        return mouseWorldPosition;
+    }
+}
