22a1e18f7fbbbe11e132dfc9569f1f8d7a295320
NicolasWiedel <nicolaswiedel@googlemail.com>
2023-09-07 08:43:25 +0200

    9.1. Cinemachine Camera

---
+++ BuilderDefender/Assets/Scripts/CameraHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraHandler : MonoBehaviour
+{
+    [SerializeField]
+    private CinemachineVirtualCamera cinemachineVirtualCamera;
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        Vector3 moveDir = new Vector3(x, y).normalized;
+        float moveSpeed = 5f;
+
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
+    }
+}
