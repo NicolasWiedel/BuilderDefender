232e7d982bd55272a9df2a323f4b8a4bdfbd655a
nicolaswiedel <nicolaswiedel@googlemail.com>
2023-09-07 18:23:27 +0200

    10. Gui Buildings

--- BuilderDefender/Assets/Scripts/BuildingManager.cs
+++ BuilderDefender/Assets/Scripts/BuildingManager.cs
@@ -1,17 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class BuildingManager : MonoBehaviour
 {
+    public static BuildingManager Instance { get; private set; }
+
     private Camera mainCamera;
     private BuildingTypeListSO buildingTypeList;
-    private BuildingTypeSO buildingType;
+    private BuildingTypeSO activeBuildingType;
 
     private void Awake()
     {
+        Instance = this;
+
         buildingTypeList = Resources.Load<BuildingTypeListSO>(typeof(BuildingTypeListSO).Name);
-        buildingType = buildingTypeList.list[0];
     }
     private void Start()
     {
@@ -20,19 +24,15 @@ public class BuildingManager : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
         {
-            Instantiate(buildingType.prefab, GetMouseWorldPosition(), Quaternion.identity);
+            if (activeBuildingType != null)
+            {
+                Instantiate(activeBuildingType.prefab, GetMouseWorldPosition(), Quaternion.identity);
+            }
         }
 
-        if (Input.GetKeyDown(KeyCode.T))
-        {
-            buildingType = buildingTypeList.list[0];
-        }
-        if (Input.GetKeyDown(KeyCode.Y))
-        {
-            buildingType = buildingTypeList.list[1];
-        }
+        
     }
 
     private Vector3 GetMouseWorldPosition()
@@ -40,5 +40,15 @@ public class BuildingManager : MonoBehaviour
         Vector3 mouseWorldPosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
         mouseWorldPosition.z = 0f;
         return mouseWorldPosition;
+    }
+
+    public void SetActiveBuildingType(BuildingTypeSO buildingType)
+    {
+        activeBuildingType = buildingType;
+    }
+
+    public BuildingTypeSO GetActiveBuildingType()
+    {
+        return activeBuildingType;
     }
 }
--- BuilderDefender/Assets/Scripts/CameraHandler.cs
+++ BuilderDefender/Assets/Scripts/CameraHandler.cs
@@ -7,15 +7,43 @@ public class CameraHandler : MonoBehaviour
 {
     [SerializeField]
     private CinemachineVirtualCamera cinemachineVirtualCamera;
+
+    private float orthographicSize;
+    private float targetOrthographicSize;
+
+    private void Start()
+    {
+        orthographicSize = cinemachineVirtualCamera.m_Lens.OrthographicSize;
+        targetOrthographicSize = orthographicSize;
+    }
+
     // Update is called once per frame
     void Update()
     {
+        HandleMovement();
+        HandleZoom();
+    }
+
+    private void HandleMovement()
+    {
+        // Camera movement with Keys
         float x = Input.GetAxisRaw("Horizontal");
         float y = Input.GetAxisRaw("Vertical");
-
         Vector3 moveDir = new Vector3(x, y).normalized;
-        float moveSpeed = 5f;
-
+        float moveSpeed = 30f;
         transform.position += moveDir * moveSpeed * Time.deltaTime;
+    }
+
+    private void HandleZoom()
+    {
+        // Camera zooming
+        float zoomAmount = 2f;
+        targetOrthographicSize += -Input.mouseScrollDelta.y * zoomAmount;
+        float minOrthographicSize = 10f;
+        float maxOrthographicSize = 30f;
+        targetOrthographicSize = Mathf.Clamp(targetOrthographicSize, minOrthographicSize, maxOrthographicSize);
+        float zoomSpeed = 5f;
+        orthographicSize = Mathf.Lerp(orthographicSize, targetOrthographicSize, Time.deltaTime * zoomSpeed);
+        cinemachineVirtualCamera.m_Lens.OrthographicSize = orthographicSize;
     }
 }
